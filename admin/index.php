6fb422f66bc488222bf5c80cb66042dc76b35578
Angel Miranda <pixeldev1729@gmail.com>
2024-06-12 09:04:19 -0600

    refactor: use a different project structure

--- admin/index.php
+++ admin/index.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../css/styles.css">
-    <link rel="icon" type="image/x-icon" href="../images/favicon.ico" />
+    <link rel="icon" type="image/x-icon" href="../image/favicon.ico"/>
     <title>FugaFix | Administración</title>
 </head>
 <body>
