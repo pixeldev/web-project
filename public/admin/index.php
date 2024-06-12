276c3589cca7ca1bb3a92f680f217142dbda682d
Angel Miranda <pixeldev1729@gmail.com>
2024-06-12 13:23:35 -0600

    feat: add navigation bar

--- public/admin/index.php
+++ public/admin/index.php
@@ -3,8 +3,8 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../css/styles.css">
-    <link rel="icon" type="image/x-icon" href="../image/favicon.ico"/>
+    <link rel="stylesheet" href="css/syles.css">
+    <link rel="icon" type="image/x-icon" href="../images/favicon.ico"/>
     <title>FugaFix | Administración</title>
 </head>
 <body>
--- public/index.php
+++ public/index.php
@@ -3,19 +3,25 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/reset.css">
+    <link rel="stylesheet" href="css/normalize.css">
     <link rel="stylesheet" href="css/styles.css">
-    <link rel="icon" type="image/x-icon" href="image/favicon.ico"/>
+    <link rel="icon" type="image/x-icon" href="images/favicon.ico"/>
     <title>FugaFix | Inicio</title>
 </head>
 <body>
-<nav>
-    <a href="index.html"><p>Inicio</p></a>
+<nav class="navbar">
+    <img class="logo-navbar" src="images/imagotipo-500x500.svg" alt="FugaFix Imagotipo">
+    <a href="index.php"><p>Inicio</p></a>
     <a href="user/service/request-service.html"><p>Hacer Solicitud</p></a>
     <a href="user/service/requests-status.html"><p>Estado de las solicitudes</p></a>
     <a href="user/account/account.html"><p>Cuenta</p></a>
 </nav>
 <section>
-    <h1>Esta le toca A Fergo</h1>
+    <h1>Esta le toca A Fergoasd</h1>
 </section>
+<footer>
+    <p>© 2024 FugaFix. Todos los derechos reservados.</p>
+</footer>
 </body>
 </html>
