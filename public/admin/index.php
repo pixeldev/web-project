f39f4b0f48519d7d0d9e8d18434f31cdc9d3d684
Angel Miranda <pixeldev1729@gmail.com>
2024-06-13 07:26:06 -0600

    feat: import poppins and use poppins light for body

--- public/admin/index.php
+++ public/admin/index.php
@@ -3,13 +3,22 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="css/syles.css">
+    <link rel="stylesheet" href="../css/reset.css">
+    <link rel="stylesheet" href="../css/normalize.css">
+    <link rel="stylesheet" href="../css/styles.css">
+    <link rel="stylesheet" href="css/styles.css">
     <link rel="icon" type="image/x-icon" href="../images/favicon.ico"/>
     <title>Fugafix | Administración</title>
 </head>
 <body>
-<header>
-
+<header class="header">
+    <img class="logo-navbar" src="../images/imagotipo-500x500.svg" alt="Fugafix Imagotipo">
+    <button class="red-button">
+        <a href="../index.php">Cerrar sesión</a>
+    </button>
 </header>
+<footer>
+
+</footer>
 </body>
 </html>
