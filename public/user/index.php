35050947a6eb41beaced6d2235bfc811faae695d
Angel Miranda <pixeldev1729@gmail.com>
2024-06-13 15:57:45 -0600

    feat: implement a lot of things

---
+++ public/user/index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/reset.css">
+    <link rel="stylesheet" href="../css/normalize.css">
+    <link rel="stylesheet" href="../css/styles.css">
+    <link rel="icon" type="image/x-icon" href="../images/favicon.ico"/>
+    <title>Fugafix | Inicio</title>
+</head>
+<body>
+<header class="header">
+    <img class="logo-navbar" src="../images/imagotipo-500x500.svg" alt="Fugafix Imagotipo">
+    <a href="index.php"><p>Inicio</p></a>
+    <a href="service/request-service.html"><p>Hacer Solicitud</p></a>
+    <a href="service/requests-status.html"><p>Estado de las solicitudes</p></a>
+    <a href="account/account.html"><p>Cuenta</p></a>
+</header>
+<section>
+    <h1>Esta le toca A Fergoasd</h1>
+</section>
+<footer>
+    <p>© 2024 Fugafix. Todos los derechos reservados.</p>
+</footer>
+</body>
+</html>
