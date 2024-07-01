765501034af28d919f53a597aa85fb03b26dc0bf
Angel Miranda <pixeldev1729@gmail.com>
2024-06-30 20:02:22 -0600

    feat: add admin account

--- backend/src/database/script.sql
+++ backend/src/database/script.sql
@@ -142,15 +142,6 @@ CREATE TABLE IF NOT EXISTS servicio_solicitud
     FOREIGN KEY (tipo_servicio) REFERENCES tipo_servicio (id)
 );
 
--- Cuenta de administrador
-INSERT INTO persona_direccion (calle, colonia, numero_ext, alcaldia, codigo_postal)
-VALUES ('Lago Silverio', 'Anáhuac I Secc', '253', 11, '11320');
-
-INSERT INTO persona (nombres, apellido_paterno, apellido_materno, email, contrasena, telefono, direccion_id, rol)
-VALUES ('Julio Darikson', 'Mazariegos', 'Aguilar', 'admin@fugafix.com',
-        '7fcf4ba391c48784edde599889d6e3f1e47a27db36ecc050cc92f259bfac38afad2c68a1ae804d77075e8fb722503f3eca2b2c1006ee6f6c7b7628cb45fffd1d',
-        '5512345678', 1, 'admin');
-
 INSERT INTO persona_direccion (calle, colonia, numero_ext, alcaldia, codigo_postal)
 VALUES
     ('Calle 1', 'Colonia A', '100', 1, '01000'), -- Zona 1
@@ -196,3 +187,9 @@ SELECT id FROM persona WHERE rol = 'tecnico';
 
 INSERT INTO usuario (id)
 SELECT id FROM persona WHERE rol = 'usuario';
+
+INSERT INTO persona_direccion(calle, colonia, numero_ext, alcaldia, codigo_postal)
+VALUES ('Lago Silverio', 'Anáhuac I Secc', '253', 11, '11320');
+
+INSERT INTO persona(nombres, apellido_paterno, apellido_materno, email, contrasena, telefono, direccion_id, rol)
+VALUES ('Julio Darikson', 'Mazariegos', 'Aguilar', 'admin@fugafix.com', 'c7ad44cbad762a5da0a452f9e854fdc1e0e7a52a38015f23f3eab1d80b931dd472634dfac71cd34ebc35d16ab7fb8a90c81f975113d6c7538dc69dd8de9077ec', '5512345678', 5, 'admin');
